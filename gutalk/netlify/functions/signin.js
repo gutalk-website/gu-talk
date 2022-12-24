a358fe9be159b9a803a0a9bd624483c132578e22
xgugugu <85472190+xgugugu@users.noreply.github.com>
2022-12-24 17:41:31 +0800

    Update signin.js

--- gutalk/netlify/functions/signin.js
+++ gutalk/netlify/functions/signin.js
@@ -7,13 +7,12 @@ const CLIENT_SECRET = '733e9c3cbfbf1c48a4243cc5d40ce1305874f038';
 exports.handler = async (event, context) => {
     let response;
     try {
-        let json = JSON.parse(event.body);
         response = await fetch(API_ENDPOINT, {
             method: 'POST',
             body: JSON.stringify({
                 client_id: CLIENT_ID,
                 client_secret: CLIENT_SECRET,
-                code: json.code
+                code: event.queryStringParameters.code
             }),
             headers: {
                 accept: 'application/json'
