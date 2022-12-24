5306a255bfec97338601253ca2521ea4f812b9c9
xgugugu <85472190+xgugugu@users.noreply.github.com>
2022-12-24 20:01:35 +0800

    Update signin.js

--- gutalk/netlify/functions/signin.js
+++ gutalk/netlify/functions/signin.js
@@ -2,18 +2,14 @@ const fetch = require('node-fetch');
 
 const API_ENDPOINT = 'https://github.com/login/oauth/access_token';
 const CLIENT_ID = '10563cad7a654a79848c';
-const CLIENT_SECRET = '733e9c3cbfbf1c48a4243cc5d40ce1305874f038';
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
 
 exports.handler = async (event, context) => {
     let response;
     try {
         response = await fetch(API_ENDPOINT, {
             method: 'POST',
-            body: JSON.stringify({
-                client_id: CLIENT_ID,
-                client_secret: CLIENT_SECRET,
-                code: event.queryStringParameters.code
-            }),
+            body: `client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${event.queryStringParameters.code}`,
             headers: {
                 accept: 'application/json'
             }
@@ -28,8 +24,6 @@ exports.handler = async (event, context) => {
     }
     return {
         statusCode: 200,
-        body: JSON.stringify({
-            data: response
-        })
+        body: response
     };
 }
